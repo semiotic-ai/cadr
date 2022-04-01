f4dc89c60f4ae8032cc73e59267ddaa65f026684
Anirudh A. Patel <anirudh@semiotic.ai>
2022-04-01 16:09:59 -0700

    Updated license date

--- src/cadr/td3.py
+++ src/cadr/td3.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 Semiotic AI, Inc.
+Copyright 2022 Semiotic AI, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
